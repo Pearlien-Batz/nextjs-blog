a612f4959609d94d4cb709e4a7288749d34a729a
Pearlien <melissapbutler@gmail.com>
2023-08-23 20:41:33 -0700

    modified:   components/Navbar.js 	new file:   components/Navbar.module.css 	modified:   public/images/profile.jpg

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,20 @@
 import Link from "next/link";
+import styles from "./Navbar.module.css"; // Import your CSS module
 
 const links = [
   { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/blog", label: "Blog" },
-  { href: "/contact", label: "Contact" },
+  { href: "/", label: "About" },
+  { href: "/", label: "Blog" },
+  { href: "/", label: "Shop" },
+  { href: "/", label: "Contact" },
 ];
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className={styles.navbar}> {/* Apply the CSS class for styling */}
+      <ul className={styles.navList}> {/* Apply the CSS class for styling */}
         {links.map((link) => (
-          <li key={link.href}>
+          <li key={link.href} className={styles.navItem}> {/* Apply the CSS class for styling */}
             <Link href={link.href}>
               {link.label}
             </Link>
